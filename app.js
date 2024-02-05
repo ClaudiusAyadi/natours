f5cee28f3a35afd1271937d482456709b1fcfbbc
ClaudiusAyadi <ayaclaudius@gmail.com>
2024-02-05 12:27:35 +0100

    instantiate the express app

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
-// Start express app
+// Instantiate the express app
 const app = express();
 
 app.set('view engine', 'pug');
