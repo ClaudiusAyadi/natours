e1854b5f4fbb9544c5b00f3bb85a8771591f5554
ClaudiusAyadi <ayaclaudius@gmail.com>
2024-02-17 11:28:25 +0100

    fix: Stripe error

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,8 +59,9 @@ exports.paid = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
 	const tour = session.client_reference_id;
 	const user = (await User.findOne({ email: session.customer_email })).id;
-	const price = session.line_items[0].price_data.unit_amount / 100;
-	await Booking.create({ tour, user, price });
+	console.log(session.line_items[0]);
+	// const price = session.line_items[0].price_data.unit_amount / 100;
+	await Booking.create({ tour, user });
 };
 
 exports.checkout = catchAsync(async (req, res, next) => {
@@ -76,7 +77,6 @@ exports.checkout = catchAsync(async (req, res, next) => {
 		return res.status(400).send(`Webhook Error: ${err.message}`);
 	}
 
-	console.log(`Unhandled event type ${event.type}`);
 	if (event.type === 'checkout.session.completed')
 		createBookingCheckout(event.data.object);
 	res.status(200).json({ received: true });
