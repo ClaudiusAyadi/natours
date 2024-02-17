6564f416e5af7937e775adfe91bff560e34c9c3d
ClaudiusAyadi <ayaclaudius@gmail.com>
2024-02-17 13:35:12 +0100

    fix: salvation at last! stripe fixed!

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,10 +59,8 @@ exports.paid = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
 	const tour = session.client_reference_id;
 	const user = (await User.findOne({ email: session.customer_email })).id;
-	console.log(session);
-	console.log(tour, user);
-	// const price = session.display_items[0].amount / 100;
-	// await Booking.create({ tour, user });
+	const price = session.amount_total / 100;
+	await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
