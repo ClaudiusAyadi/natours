6a562fa1d20d61d6e33f1bdef012a793361229af
ClaudiusAyadi <ayaclaudius@gmail.com>
2024-02-17 12:33:01 +0100

    still on it

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,10 +59,10 @@ exports.paid = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
 	const tour = session.client_reference_id;
 	const user = (await User.findOne({ email: session.customer_email })).id;
-	console.log(session.line_items[0]);
-	console.log(session.display_items[0]);
-	const price = session.display_items[0].amount / 100;
-	await Booking.create({ tour, user, price });
+	console.log(session.line_items);
+	console.log(session.display_items);
+	// const price = session.display_items[0].amount / 100;
+	await Booking.create({ tour, user });
 };
 
 exports.checkout = (req, res, next) => {
